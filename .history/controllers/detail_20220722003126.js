346e83e3e61fc11e606e59db5dfec6efba99011c
Tuyet-Vy <nhuochanvu@gmail.com>
2022-07-23 00:22:13 +0700

    initialize project

---
+++ .history/controllers/detail_20220722003126.js
@@ -0,0 +1,107 @@
+function getProduct(){
+
+    var promise = axios ({
+        url:'https://shop.cyberlearn.vn/api/Product',
+        method:'GET',
+    });
+
+    promise.then(function(result){
+        renderProduct(result.data.content,'tableRender');
+    })
+}
+
+
+function renderProduct (arrFrame,idBody){
+    var htmlContent = '';
+    for (var index = 0; index < arrFrame.length; index ++) {
+        var frame = arrFrame[index];
+        // console.log(frame)
+
+        htmlContent += `
+        <div class="frame m-4">
+            <div class="frame-item">
+                <div class="frame-item-top">
+                    <img class="frameimg"src="${frame.image}" alt="...">
+                    <p>${frame.name}</p>
+                    <p>${frame.shortDescription}</p>
+                </div>
+                <div class="frame-item-bottom">
+                    <a class="btbuy" onclick="buyframe('${frame.id}')" type="button">Buy now</a>
+                    <a class="btprice" type="button">${frame.price}$</a>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    document.getElementById(idBody).innerHTML = htmlContent;
+}
+
+
+
+
+
+
+function getProductDetail() {
+
+    var promise = axios ({
+        url:'https://shop.cyberlearn.vn/api/Product/getbyid?id=4',
+        method:'GET',
+    });
+
+    promise.then(function(result){
+        renderProductDetail(result.data.content,'detailProduct');
+    })
+}
+
+
+function renderProductDetail (arrDetaild,idBody){
+    var htmlContent = '';
+    for (var index = 0; index < arrDetaild.length; index ++) {
+        var frame = arrDetaild[index];
+        // console.log(frame)
+
+        htmlContent += `
+        <div class="frame m-4">
+            <div class="frame-item">
+                <div class="frame-item-top">
+                    <img class="frameimg"src="${frame.image}" alt="...">
+                    <p>${frame.name}</p>
+                    <p>${frame.shortDescription}</p>
+                </div>
+                <div class="frame-item-bottom">
+                    <a class="btbuy" onclick="buyframe('${frame.id}')" type="button">Buy now</a>
+                    <a class="btprice" type="button">${frame.price}$</a>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    document.getElementById(idBody).innerHTML = htmlContent;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function (){
+    getProduct();
+    getProductDetail();
+ }
+
+ 
---
+++ .history/controllers/detail_20220722003503.js
@@ -0,0 +1,47 @@
+
+function getDanhSachDoiTuong(){
+
+    var promise = axios ({
+        url:'https://shop.cyberlearn.vn/api/Product',
+        method:'GET',
+    });
+
+    promise.then(function(result){
+        // console.log(result.data.content)
+
+        renderDanhSachDoiTuong(result.data.content,'rendertable');
+    })
+}
+
+
+function renderDanhSachDoiTuong (arrDoiTuong,idBody){
+    var htmlContent = '';
+    for (var index = 0; index < arrDoiTuong.length; index ++) {
+        var doiTuong = arrDoiTuong[index];
+        // console.log(doiTuong)
+
+        htmlContent += `
+        <div class="rendertable-item">
+            <div class="rendertable-item-inner"">
+                <div class="rendertable-item-inner-top">
+                    <img src="${doiTuong.image}" alt="...">
+                </div>
+                <div class="rendertable-item-inner-mid">
+                    <p>${doiTuong.alias}</p>
+                    <p><span>${doiTuong.shortDescription}</span></p>
+                </div>
+                <div class="rendertable-item-inner-bottom">
+                    <button class="btn btn-warning" onclick="muaDoiTuong('${doiTuong.id}')">Buy now</button>
+                    <button class="btn btn-secondary btn-gia" type="button">${doiTuong.price}$</button>
+                </div>
+            </div>
+        </div>
+        `
+    }
+    document.getElementById(idBody).innerHTML = htmlContent;
+}
+
+
+window.onload = function (){
+    getDanhSachDoiTuong();
+}
